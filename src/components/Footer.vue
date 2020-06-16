38d1235b4dd076cbca790256b8abf23e4d4c3746
Jeff <qaawssedd456@gmail.com>
2020-06-17 01:23:42 +0800

    Update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,42 +1,6 @@
 <template>
   <footer>
-    <div class="sub">
-      <div class="container">
-        <div class="logo">
-          <img src="../assets/img/logo-light.png" alt />
-          <p>訂閱你我的甜蜜郵件</p>
-        </div>
-        <div class="email">
-          <div class="icon">
-            <i class="fas fa-envelope"></i>
-          </div>
-          <input type="text" id="email" name="email" />
-          <label for="email">
-            <i class="fas fa-angle-right"></i>
-          </label>
-        </div>
-      </div>
-    </div>
-    <div class="bottom">
-      <div class="left">
-        <div class="logo">
-          <img src="../assets/img/logotype.png" alt="logo" />
-        </div>
-        <div class="info">
-          <p>07-1234-5678</p>
-          <p>sweetaste@email.com</p>
-          <p>800 高雄市新興區幸福路 520 號</p>
-        </div>
-        <div class="icon">
-          <img src="../assets/img/ic-facebook.png" alt="fb" />
-          <img src="../assets/img/ic-line@.png" alt="line" />
-        </div>
-      </div>
-      <div class="right">
-        <img src="../assets/img/sm-今天是個吃甜點的好日子.png" alt />
-        <p>© 2020 Sweetaste* All Rights Reserved</p>
-      </div>
-    </div>
+    Footer
   </footer>
 </template>
 
@@ -44,5 +8,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,6 @@
 <template>
   <header>
-    <nav class="pc_menu">
-      <div class="logo">
-        <a href="index.html">
-          <img alt="Vue logo" src="../assets/img/logo-all-dark.png" />
-        </a>
-      </div>
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/product">甜點</router-link>
-        </li>
-        <li>
-          <router-link to="/login">登入</router-link>
-        </li>
-        <li>
-          <router-link to="/cart">
-            <i class="fas fa-shopping-cart"></i>
-          </router-link>
-        </li>
-      </ul>
-    </nav>
+   Header
   </header>
 </template>
 
